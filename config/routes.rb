dbdd6c8e6f6a16309a3d6262e481b8dfae12c59d
Yas la menace <yasbernays@gmail.com>
2016-11-08 14:02:01 +0100

    Add correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'restaurant#index'
+  get "tasks", to: "tasks#index"
+  root 'task#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :restaurants
+resources :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108105257) do
+ActiveRecord::Schema.define(version: 20161108125154) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.integer  "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
